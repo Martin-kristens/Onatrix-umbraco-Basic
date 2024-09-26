c9c06dfa0f0a79cca2923a973af333910b90cc4b
Martin <martinkristensen12@gmail.com>
2024-09-26 18:19:23 +0200

    form at Contact page done, started form at service details page, styling left

---
+++ Onatrix-umbraco/Models/ContactFormModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Onatrix_umbraco.Models;
+
+public class ContactFormModel
+{
+    public string Name { get; set; } = null!;
+    public string Phone { get; set; } = null!;
+    public string Email { get; set; } = null!;
+	public string SelectOption { get; set; } = null!;
+    public string Message { get; set; } = null!;
+}
